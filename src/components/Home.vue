88475205f46b67a1f4cb13378efa6b0273fc5139
kmyles82 <kmyles82@gmail.com>
2020-06-20 08:38:07 -0500

    added fuctionality to loop through menu items

---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="background">
+    <div class="background-overlay">
+      <h2>Welcome To Pizza Planet</h2>
+      <img src="../assets/roller.png" alt="roller" />
+      <h3>Feeling Hungry?</h3>
+      <button class="order_btn">Let's Order!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "apphome"
+};
+</script>
+
+<style>
+h2,
+h3 {
+  margin: 2%;
+}
+
+.background {
+  background: url("../assets/dough.jpg") center;
+  background-size: cover;
+  height: 30vw;
+  display: flex;
+  text-align: center;
+  align-items: center;
+}
+
+.background-overlay {
+  display: flex;
+  flex-direction: column;
+  background: #f1e6da;
+  opacity: 0.8;
+  width: 50%;
+  margin: 0 auto;
+  padding: 5px;
+  align-items: center;
+}
+
+.background-overlay img {
+  max-width: 15%;
+}
+
+.order_btn {
+  background: #f79f38;
+  border-radius: 3px;
+  margin-bottom: 10px;
+  padding: 10px;
+  cursor: pointer;
+  border: none;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="menu_wrapper">
+    <!-- menu -->
+    <div class="menu">
+      <h3>~ Authentic Handmade Pizza ~</h3>
+      <table>
+        <tbody v-for="(item, index) in getMenuItems" :key="index">
+          <tr>
+            <td>
+              <strong>~ {{ item.name }} ~</strong>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <small>{{ item.description }}</small>
+            </td>
+          </tr>
+          <tr v-for="(option, index) in item.options" :key="index">
+            <td>{{ option.size }}</td>
+            <td>${{ option.price }}</td>
+            <td>
+              <button type="button" class="btn_green">+</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "appMenu",
+  data() {
+    return {
+      getMenuItems: {
+        1: {
+          name: "Margherita",
+          description: "A delicious tomato based pizza topped with mozzarella",
+          options: [
+            {
+              size: 9,
+              price: 6.95
+            },
+            {
+              size: 12,
+              price: 10.95
+            }
+          ]
+        },
+        2: {
+          name: "Pepperoni",
+          description:
+            "A delicious tomato based pizza topped with mozzarella and pepperoni",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        },
+        3: {
+          name: "Ham and Pineapple",
+          description:
+            "A delicious tomato based pizza topped with mozzarella, ham and pineapple",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  text-align: center;
+}
+
+.menu_wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.menu {
+  background: #f1e6da;
+  border-radius: 3px;
+  height: 100vh;
+  margin: 10px;
+  padding: 10px;
+}
+
+@media screen and (min-width: 900px) {
+  .menu_wrapper {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .menu {
+    width: 65vw;
+  }
+}
+</style>
